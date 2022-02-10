3b0dd0ea31ca38916ab2e40c9a562d41743b30ad
nickyiliwang <nickyiliwang@gmail.com>
2022-02-10 12:21:59 -0500

    removed duplicating code

--- Client/Archive/temp-humid.cpp
+++ Client/Archive/temp-humid.cpp
@@ -1,24 +1,11 @@
-// We are sending this data through iot analytics pipline
-// it pulls data from "outTopic", we are sending it like [{temperature: 12,
-// humidity: 12}] (note the array) We are taking this data and transforming it
-// with an lambda function that addes "serversideTimeStamp" , lambda function
-// called: node_iot_esp32 *important iot analytics starts without permission to
-// invoke lambda functions we had to add this to give it permissions:
-
-// aws lambda add-permission --function-name node_iot_esp32
-// --action lambda:InvokeFunction --statement-id iotanalytics
-// --principal iotanalytics.amazonaws.com --profile iamadmin
-
-// this sketch generates all the mock data for iot analytics and jupitor
-// notebook/sagemake
-
-// Using Dataset content delivery rules in aws iot ana to save data in csv in s3
-// dataset/mydataset/!{iotanalytics:scheduleTime}/!{iotanalytics:versionId}.csv
-// buckets needed to be public
-// bucket policy
-// {"Version":"2012-10-17","Statement":[{"Sid":"PublicRead","Effect":"Allow","Principal":"*","Action":"s3:GetObject","Resource":"arn:aws:s3:::iot-analytics-temperature-humidity-esp32-nickyiliwang/*"}]}
-// cors
-// [{"AllowedHeaders":["Authorization"],"AllowedMethods":["GET"],"AllowedOrigins":["*"],"ExposeHeaders":[],"MaxAgeSeconds":3000}]
+/* ESP32 AWS IoT
+ *
+ * Simplest possible example (that I could come up with) of using an ESP32 with
+ * AWS IoT.
+ *
+ * Author: Anthony Elder
+ * License: Apache License v2
+ */
 
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
@@ -57,6 +44,21 @@ void setup() {
   wiFiClient.setPrivateKey(private_pem_key);
 }
 
+void pubSubCheckConnect() {
+  if (!pubSubClient.connected()) {
+    Serial.print("PubSubClient connecting to: ");
+    Serial.print(awsEndpoint);
+    while (!pubSubClient.connected()) {
+      Serial.print(".");
+      pubSubClient.connect("ESPthingXXXX");
+      delay(1000);
+    }
+    Serial.println(" connected");
+    pubSubClient.subscribe("inTopic");
+  }
+  pubSubClient.loop();
+}
+
 unsigned long lastPublish;
 int msgCount;
 
@@ -90,19 +92,4 @@ void msgReceived(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-}
-
-void pubSubCheckConnect() {
-  if (!pubSubClient.connected()) {
-    Serial.print("PubSubClient connecting to: ");
-    Serial.print(awsEndpoint);
-    while (!pubSubClient.connected()) {
-      Serial.print(".");
-      pubSubClient.connect("ESPthingXXXX");
-      delay(1000);
-    }
-    Serial.println(" connected");
-    pubSubClient.subscribe("inTopic");
-  }
-  pubSubClient.loop();
 }
